feba4efab6d7d6f496aebb783ebca60230b49bd0
Anna Natorilla <annagaiel@gmail.com>
2016-05-23 18:46:08 -0700

    add greeting text on the array

--- app/assets/javascripts/exampleCtrl.js
+++ app/assets/javascripts/exampleCtrl.js
@@ -3,5 +3,15 @@
   angular.module("app").controller("exampleCtrl",
     function($scope){
       $scope.messages = ["Hello", "Hi", "GoodBye"];
+
+      $scope.alertMe = function(message){
+        alert(message);
+      }
+
+      $scope.addMessage = function(message){
+        $scope.messages.push(message)
+      }
+
+      window.$scope = $scope;
   });
 })();
